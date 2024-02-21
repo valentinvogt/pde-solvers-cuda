740aaa8ee68d5884f803fc541483454c05fdbbe6
Louis Hurschler <louis.hurschler@gmx.ch>
2024-02-21 11:40:01 +0100

    fix shell script for euler

---
+++ euler_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+module load gcc/11.4.0
+module load cmake/3.26.3
+module load cuda/12.1.1
+
+cmake -B build
+make -C build
+
+./build/satt
--- modules.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-module load cmake/3.26.3 cuda/12.1.1
