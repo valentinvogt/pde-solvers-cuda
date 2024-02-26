801e349c7b0f8cb3f994dc19c22757fb5b8784f8
Louis Hurschler <louis.hurschler@gmx.ch>
2024-02-26 16:15:15 +0100

    enable to compile without CUDA

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # pde-solvers-cuda
+
+
+## Requirements
+Currently, only UNIX based operating systems are supported. Moreover, you need to have the following installed on your machine:
+* C++ compiler (e.g. gcc, clang)
+* CMake (at least VERSION 3.18)
+* CUDA (not strictly needed)
+* git (not strictly needed, you could also download the repo as .zip file)
+
+## Getting startet
+You can build pds-solvers-cuda as follows
+
+'''
+git clone https://github.com/LouisHurschler/pde-solvers-cuda.git  
+cd pde-solvers-cuda
+mkdir build
+cmkae -B build -DENABLE_CUDA={0,1}
+'''
+Note that ENABLE_CUDA is set OFF by default
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "NPDE code";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      devShell.${system} = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+        cmake
+        clang-tools
+        ];
+        buildInputs = with pkgs; [ 
+        ];
+
+        CPATH = pkgs.lib.makeSearchPathOutput "dev" "include" buildInputs;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+      };
+    };
+}
