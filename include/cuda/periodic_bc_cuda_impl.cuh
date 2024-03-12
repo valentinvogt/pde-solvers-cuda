4996205961c23baec994fcbc532cfad9cfab51d7
Louis Hurschler <louis.hurschler@gmx.ch>
2024-03-12 16:03:56 +0100

    test

--- include/cuda/periodic_bc_cuda_impl.cuh
+++ include/cuda/periodic_bc_cuda_impl.cuh
@@ -3,13 +3,24 @@
 
 #include <zisa/memory/array.hpp>
 
+#ifndef THREAD_DIMS
+#define THREAD_DIMS 1024
+
 template <typename Scalar>
 __global__ void
 periodic_bc_cuda_kernel(zisa::array<Scalar, 2> &data,
                      unsigned n_ghost_cells_x,
-                     unsigned n_ghost_cells_y) {
-  // TODO
-  printf("Hello from GPU");
+                     unsigned n_ghost_cells_y, unsigned data_size) {
+  const unsigned idx = blockIdx.x * THREAD_DIMS + threadIdx.x;
+  if (idx < data_size) {
+    if (idx < n_ghost_cells_x * data.shape(1)) {
+      // upper boundary
+      const unsigned x_idx = idx / data.shape(1);
+      const unsigned y_idx = idx - data.shape(1) * x_idx;
+      const unsigned x_idx_to_copy = data.shape(0) - 2 * n_ghost_cells_x + x_idx;
+      data(x_idx, y_idx) = data(x_idx_to_copy, y_idx);
+    }
+  }
 }
 
 template <typename Scalar>
@@ -17,9 +28,11 @@ void periodic_bc_cuda(zisa::array<Scalar, 2> &data,
                    unsigned n_ghost_cells_x,
                    unsigned n_ghost_cells_y) {
 #if CUDA_AVAILABLE
-  const int thread_dims = 1024;
-  const int block_dims = std::ceil((data.shape(0) * data.shape(1)) / thread_dims);
-  periodic_bc_cuda_kernel<<<block_dims, thread_dims>>>(data, n_ghost_cells_x, n_ghost_cells_y);
+  const unsigned thread_dims = THTHREAD_DIMS;
+  // size of whole boundary where periodic bc has to be applied
+  const unsigned data_size = data.shape(1) * n_ghost_cells_x * 2 + (data.shape(0) - 2 * n_ghost_cells_x) * n_ghost_cells_y * 2;
+  const unsigned block_dims = std::ceil(data_size) / thread_dims);
+  periodic_bc_cuda_kernel<<<block_dims, thread_dims>>>(data, n_ghost_cells_x, n_ghost_cells_y, data_size);
   cudaDeviceSynchronize();
 #endif // CUDA_AVAILABLE
 }
