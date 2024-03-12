3a667239827b8bf7130c8f27852e2c7029942ae5
Louis Hurschler <louis.hurschler@gmx.ch>
2024-03-12 12:53:02 +0100

    bug fixes

--- include/cuda/dirichlet_bc_cuda.hpp
+++ include/cuda/dirichlet_bc_cuda.hpp
@@ -8,7 +8,7 @@ void dirichlet_bc_cuda(zisa::array<Scalar, 2> &data, unsigned n_ghost_cells_x,
                      unsigned n_ghost_cells_y, Scalar value);
 
 #define PDE_SOLVERS_CUDA_INSTANCIATE_DIRICHLET_BC_CUDA(TYPE)  \
-  extern template void convolve_cuda<TYPE>(                 \
+  extern template void dirichlet_bc_cuda<TYPE>(                 \
     zisa::array<TYPE, 2> &, unsigned, unsigned, TYPE);            \
 
 PDE_SOLVERS_CUDA_INSTANCIATE_DIRICHLET_BC_CUDA(float)
