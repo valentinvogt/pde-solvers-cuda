14d432f3dfb21b9f621699c22fc7b64dee9e0a7d
Louis Hurschler <louis.hurschler@gmx.ch>
2024-03-12 13:22:05 +0100

    bug fixes

--- src/cuda/periodic_bc_cuda.cu
+++ src/cuda/periodic_bc_cuda.cu
@@ -1,11 +1,6 @@
 #include <cuda/periodic_bc_cuda.hpp>
 #include <cuda/periodic_bc_cuda_impl.cuh>
 
-#ifndef PERIODIC_BC_CUDA_H_
-#define PERIODIC_BC_CUDA_H_
-
-#include <zisa/memory/array.hpp>
-
 template <typename Scalar>
 void periodic_bc_cuda(zisa::array<Scalar, 2> &data, unsigned n_ghost_cells_x,
                      unsigned n_ghost_cells_y);
@@ -18,4 +13,3 @@ PDE_SOLVERS_CUDA_INSTANCIATE_PERIODIC_BC_CUDA(float)
 PDE_SOLVERS_CUDA_INSTANCIATE_PERIODIC_BC_CUDA(double)
 
 #undef PDE_SOLVERS_CUDA_INSTANCIATE_PERIODIC_BC_CUDA
-#endif // PERIODIC_BC_CUDA_H_
