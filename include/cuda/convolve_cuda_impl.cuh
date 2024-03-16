307c4ac607cd3810283df076cce3e47085cc5d20
Louis Hurschler <louis.hurschler@gmx.ch>
2024-03-16 23:07:08 +0100

    commit for euler

--- include/cuda/convolve_cuda_impl.cuh
+++ include/cuda/convolve_cuda_impl.cuh
@@ -14,34 +14,28 @@ __global__ void
 convolve_cuda_kernel(zisa::array_view<Scalar, 2> dst,
                      zisa::array_const_view<Scalar, 2> src,
                      zisa::array_const_view<Scalar, 2> kernel) {
+  // TODO
   const int linear_idx = threadIdx.x + blockIdx.x * THREAD_DIMS; 
-  if (linear_idx < src.shape(0) * src.shape(1)) { 
-        dst[linear_idx] = src[linear_idx]; 
-  } 
-  return;
+
   const int ghost_x = kernel.shape(0) / 2;
   const int ghost_y = kernel.shape(1) / 2;
 
   const int Nx = src.shape(0) - 2 * ghost_x;
   const int Ny = src.shape(1) - 2 * ghost_y;
 
-  const int linear_idx = threadIdx.x + blockIdx.x * THREAD_DIMS;
-  if (linear_idx >= Nx * Ny) {
-    return;
-  }
-  const int x_idx = ghost_x +  linear_idx / Ny;
-  const int y_idx = ghost_y + linear_idx % Ny;
-  printf("convolve_cuda_kernel reached with x_idx = %u, y_idx = %u\n", x_idx, y_idx);
-  return;
-  dst(x_idx, y_idx) = 0.;
-  for (int di = -ghost_x; di <= ghost_x; di++) {
-    for (int dj = -ghost_y; dj <= ghost_y; dj++) {
-      if (kernel(ghost_x + di, ghost_y + dj) != 0) {
-        dst(x_idx, y_idx) += kernel(ghost_x + di, ghost_y + dj) * src(x_idx + di, y_idx + dj);
+  if (linear_idx < Nx * Ny) { 
+    const int x_idx = ghost_x +  linear_idx / Ny;
+    const int y_idx = ghost_y + linear_idx % Ny;
+
+    dst(x_idx, y_idx) = 0; 
+    for (int di = -ghost_x; di <= ghost_x; di++) {
+      for (int dj = -ghost_y; dj <= ghost_y; dj++) {
+        if (kernel(ghost_x + di, ghost_y + dj) != 0) {
+          dst(x_idx, y_idx) += kernel(ghost_x + di, ghost_y + dj) * src(x_idx + di, y_idx + dj);
+        }
       }
     }
-  }
-  printf("convolve_cuda_kernel reached with x_idx = %u, y_idx = %u\n", x_idx, y_idx);
+  } 
 }
 
 template <typename Scalar>
