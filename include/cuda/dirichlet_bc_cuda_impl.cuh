70cedd144f88402fc75518094175df21c80eb4f4
Louis Hurschler <louis.hurschler@gmx.ch>
2024-03-16 20:27:40 +0100

    commit for euler

--- include/cuda/dirichlet_bc_cuda_impl.cuh
+++ include/cuda/dirichlet_bc_cuda_impl.cuh
@@ -25,7 +25,7 @@ void dirichlet_bc_cuda(zisa::array_view<Scalar, 2> data,
 #if CUDA_AVAILABLE
   const int thread_dims = THREAD_DIMS;
   const int block_dims = std::ceil((double)(data.shape(0) * data.shape(1)) / thread_dims);
-  dirichlet_bc_cuda_kernel<<<block_dims, thread_dims>>>(data, bc, n_ghost_cells_x, n_ghost_cells_y, value);
+  dirichlet_bc_cuda_kernel<<<block_dims, thread_dims>>>(data, bc, n_ghost_cells_x, n_ghost_cells_y);
   const auto error = cudaDeviceSynchronize();
   if (error != cudaSuccess) {
     std::cout << "Error in convolve_cuda: " << cudaGetErrorString(error) << std::endl;
