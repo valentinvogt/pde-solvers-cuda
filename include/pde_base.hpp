8f92504278273374670c25a5dd4739bd1108643a
Louis Hurschler <louis.hurschler@gmx.ch>
2024-03-20 11:08:23 +0100

    commit for euler

--- include/pde_base.hpp
+++ include/pde_base.hpp
@@ -83,9 +83,9 @@ public:
     std::cout << "bc values:" << std::endl;
     print_matrix(bc_values_);
     std::cout << "sigma values vertical:" << std::endl;
-    print_matrix(sigma_values_vertical_);
+    // print_matrix(sigma_values_vertical_);
     std::cout << "sigma values horizontal:" << std::endl;
-    print_matrix(sigma_values_horizontal_);
+    // print_matrix(sigma_values_horizontal_);
   }
 
 protected:
