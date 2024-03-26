1b9390e3e543307931d9947ba411c130410741dd
Louis Hurschler <louis.hurschler@gmx.ch>
2024-03-26 13:25:43 +0100

    commit for euler

--- src/cuda/add_arrays_cuda.cu
+++ src/cuda/add_arrays_cuda.cu
@@ -8,7 +8,7 @@ void add_arrays_cuda(zisa::array_view<Scalar, 2> dst,
 
 #define PDE_SOLVERS_CUDA_INSTANCIATE_ADD_ARRAYS_CUDA(TYPE)                     \
   template void add_arrays_cuda<TYPE>(                                  \
-      zisa::array_view<TYPE, 2>, zisa::array_const_view<TYPE, 2>, Scalar);
+      zisa::array_view<TYPE, 2>, zisa::array_const_view<TYPE, 2>, TYPE);
 
 PDE_SOLVERS_CUDA_INSTANCIATE_ADD_ARRAYS_CUDA(float)
 PDE_SOLVERS_CUDA_INSTANCIATE_ADD_ARRAYS_CUDA(double)
