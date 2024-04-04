caffd892d250cfb1f07f2dec824b5bdd9078d5f1
Louis Hurschler <louis.hurschler@gmx.ch>
2024-04-04 10:39:44 +0200

    fix bug in writer

--- include/io/netcdf_writer.hpp
+++ include/io/netcdf_writer.hpp
@@ -29,42 +29,9 @@ public:
                   const std::string &filename)
       : /* used for compilation (no default constructor), should be changed
            later*/
-        writer_(DummyWriter()) {
-
-    std::vector<std::tuple<std::string, std::size_t>> dims = {
-        {"member", n_members}, {"time", n_snapshots}, {"x", n_x}, {"y", n_y}};
-
-    std::vector<
-        std::tuple<std::string, std::vector<std::string>, zisa::ErasedDataType>>
-        vars;
-    std::vector<std::string> member_var{"member"};
-    vars.emplace_back("member", member_var, zisa::erase_data_type<int>());
-
-    std::vector<std::string> time_var{"time"};
-    vars.emplace_back("time", time_var, zisa::erase_data_type<Scalar>());
-
-    std::vector<std::string> x_pos_var{"x"};
-    vars.emplace_back("x_pos", x_pos_var, zisa::erase_data_type<Scalar>());
-
-    std::vector<std::string> y_pos_var{"y"};
-    vars.emplace_back("y_pos", y_pos_var, zisa::erase_data_type<Scalar>());
-
-    // create a variable for every snapshot
-    // for example, snapshot of member 2 at 3'rd timestep is called m_2_t_3
-    for (int member = 0; member < n_members; member++) {
-      for (int snapshot = 0; snapshot < n_snapshots; snapshot++) {
-        std::string append =
-            "m_" + std::to_string(member) + "_s_" + std::to_string(snapshot);
-        std::vector<std::string> function_value_var{append};
-        vars.emplace_back(append, function_value_var,
-                          zisa::erase_data_type<Scalar>());
-        dims.emplace_back(append, n_x * n_y);
-      }
-    }
-
-    zisa::NetCDFFileStructure file_structure(dims, vars);
-    writer_ = zisa::NetCDFSerialWriter(filename, file_structure);
-    std::cout << "writer created" << std::endl;
+        writer_(make_writer(n_snapshots, T, n_members, n_x, x_begin, x_end, n_y, y_begin, y_end, filename) {
+    // netcd_put_vara
+    // save in chunks
 
     // already save member, time and position arrays
     zisa::array<int, 1> members((zisa::shape_t<1>(n_members)));
@@ -104,6 +71,45 @@ public:
 
 private:
   zisa::NetCDFSerialWriter writer_;
+  zisa::NetCDFSerialWriter make_writer(unsigned int n_snapshots, Scalar T, unsigned int n_members,
+                  unsigned int n_x, Scalar x_begin, Scalar x_end,
+                  unsigned int n_y, Scalar y_begin, Scalar y_end,
+                  const std::string &filename) {
+    std::vector<std::tuple<std::string, std::size_t>> dims = {
+        {"member", n_members}, {"time", n_snapshots}, {"x", n_x}, {"y", n_y}};
+
+    std::vector<
+        std::tuple<std::string, std::vector<std::string>, zisa::ErasedDataType>>
+        vars;
+    std::vector<std::string> member_var{"member"};
+    vars.emplace_back("member", member_var, zisa::erase_data_type<int>());
+
+    std::vector<std::string> time_var{"time"};
+    vars.emplace_back("time", time_var, zisa::erase_data_type<Scalar>());
+
+    std::vector<std::string> x_pos_var{"x"};
+    vars.emplace_back("x_pos", x_pos_var, zisa::erase_data_type<Scalar>());
+
+    std::vector<std::string> y_pos_var{"y"};
+    vars.emplace_back("y_pos", y_pos_var, zisa::erase_data_type<Scalar>());
+
+    // create a variable for every snapshot
+    // for example, snapshot of member 2 at 3'rd timestep is called m_2_t_3
+    for (int member = 0; member < n_members; member++) {
+      for (int snapshot = 0; snapshot < n_snapshots; snapshot++) {
+        std::string append =
+            "m_" + std::to_string(member) + "_s_" + std::to_string(snapshot);
+        std::vector<std::string> function_value_var{append};
+        vars.emplace_back(append, function_value_var,
+                          zisa::erase_data_type<Scalar>());
+        dims.emplace_back(append, n_x * n_y);
+      }
+    }
+
+    zisa::NetCDFFileStructure file_structure(dims, vars);
+    return zisa::NetCDFSerialWriter(filename, file_structure);
+    
+  }
 };
 
 #endif // NETCDF_WRITER_HPP_
