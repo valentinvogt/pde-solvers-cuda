9b2ff30c503067a77c520c7a923726e9008f0646
Louis Hurschler <louis.hurschler@gmx.ch>
2024-04-22 15:46:41 +0200

    commit for euler

--- include/coupled_function.hpp
+++ include/coupled_function.hpp
@@ -1,26 +1,35 @@
 
-#ifndef GENERIC_FUNCTION_HPP_
-#define GENERIC_FUNCTION_HPP_
+#ifndef COUPLED_FUNCTION_HPP_
+#define COUPLED_FUNCTION_HPP_
 
 #if CUDA_AVAILABLE
 #include <cuda_runtime.h>
 #endif
 #include <zisa/memory/array.hpp>
 
-#define CREATE_ARGUMENTS_REC(1, VALUE) VALUE
-#define CREATE_ARGUMENTS_REC(SIZE, VALUE)                                      \
-  VALUE, CREATE_ARGUMENTS_REC(SIZE - 1, VALUE)
-
 // currently implemented for polynoms of max degree 2
 #include <cmath>
+
+template <int n> zisa::shape_t<n> create_shape(int value) {
+  return zisa::shape_t<n>(value);
+}
+
+template <> zisa::shape_t<1> create_shape<1>(int value) {
+  return zisa::shape_t<1>(value);
+}
+template <> zisa::shape_t<2> create_shape<2>(int value) {
+  return zisa::shape_t<2>(value, value);
+}
+template <> zisa::shape_t<3> create_shape<3>(int value) {
+  return zisa::shape_t<3>(value, value, value);
+}
 
 template <typename Scalar, int n_coupled, int max_pot> class CoupledFunction {
 public:
   CoupledFunction() {}
   CoupledFunction(zisa::array_const_view<Scalar, n_coupled> scalings)
-      : scalings_(
-            zisa::shape_t<n_coupled>(CREATE_ARGUMENTS_REC(n_coupled, max_pot),
-                                     scalings.memory_location())) {
+      : scalings_(create_shape<n_coupled>(max_pot),
+                  scalings.memory_location()) {
     zisa::copy(scalings_, scalings);
   }
 #if CUDA_AVAILABLE
@@ -28,27 +37,17 @@ public:
   // output: Scalar, f(x, y, z)
   inline __host__ __device__ Scalar
   operator()(zisa::array_const_view<Scalar, 1> x) {
-    return const_val_ + lin_val_ * x + quad_val_ * x * x +
-           exp_scale_val_ * std::exp(exp_pot_val_ * x);
+    // TODO
+    return 0;
   }
 #else
   // input: array of size n_coupled, representing all values of one position
   // output: Scalar, f(x, y, z)
   inline Scalar operator()(zisa::array_const_view<Scalar, 1> x) {
-    return const_val_ + lin_val_ * x + quad_val_ * x * x +
-           exp_scale_val_ * std::exp(exp_pot_val_ * x);
+    // TODO
+    return 0;
   }
 #endif
-
-  void set_const(Scalar value) { const_val_ = value; }
-
-  void set_lin(Scalar value) { lin_val_ = value; }
-  void set_quad(Scalar value) { quad_val_ = value; }
-
-  void set_exp(Scalar scale, Scalar pot) {
-    exp_scale_val_ = scale;
-    exp_pot_val_ = pot;
-  }
 
 private:
   // function returns sum_(i, j, k = 0)^max_pot scalings_(i, j, k) x^i y^j z^k
@@ -58,4 +57,4 @@ private:
   zisa::array<Scalar, n_coupled> scalings_;
 };
 
-#endif // GENERIC_FUNCTION_HPP_
+#endif // COUPLED_FUNCTION_HPP_
