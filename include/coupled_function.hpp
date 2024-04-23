46b47131183da6ec3e602d678a7cf292aabe890c
Louis Hurschler <louis.hurschler@gmx.ch>
2024-04-23 08:53:33 +0200

    fix for euler

--- include/coupled_function.hpp
+++ include/coupled_function.hpp
@@ -2,6 +2,7 @@
 #ifndef COUPLED_FUNCTION_HPP_
 #define COUPLED_FUNCTION_HPP_
 
+#include "zisa/memory/device_type.hpp"
 #if CUDA_AVAILABLE
 #include <cuda_runtime.h>
 #endif
@@ -13,11 +14,23 @@
 
 template <typename Scalar, int n_coupled, int max_pot> class CoupledFunction {
 public:
-  CoupledFunction() {}
+  CoupledFunction() {};
   CoupledFunction(zisa::array_const_view<Scalar, 1> scalings)
-      : scalings_(zisa::shape_t<1>((int)std::pow<int>(max_pot, n_coupled)), scalings.memory_location()) {
+      : scalings_(zisa::shape_t<1>((int)std::pow<int>(max_pot, n_coupled)), scalings.memory_location()),
+        memory_location_(scalings.memory_location()){
     zisa::copy(scalings_, scalings);
   }
+
+  CoupledFunction(const CoupledFunction &other) 
+  : scalings_(zisa::shape_t<1>(other.scalings_.size()), other.memory_location_){
+    zisa::copy(scalings_, other.scalings_);
+  }
+  
+  CoupledFunction operator=(const CoupledFunction &other) {
+    CoupledFunction tmp(scalings_.const_view());
+    return tmp;
+  }
+
 #if CUDA_AVAILABLE
   // input: array of size n_coupled, representing all values of one position
   // output: Scalar, f(x, y, z)
@@ -69,6 +82,15 @@ public:
     return result;
   }
 #endif
+  // function overloaded such that you don't have to create an
+  // array every time you call this function with n_coupled == 1
+  // could be deleted later
+  inline Scalar operator()(Scalar value)  {
+    assert(n_coupled == 1);
+    zisa::array<Scalar, 1> tmp(zisa::shape_t<1>(1), memory_location_);
+    tmp(0) = value;
+    return this->operator()(tmp.const_view());
+  }
 
 private:
   // function returns sum_(i, j, k = 0)^max_pot scalings_(i, j, k) x^i y^j z^k
@@ -76,6 +98,7 @@ private:
   // f(x, y) = scaling_(0, 0) + scaling(0, 1) * y + scaling(1, 0) * x +
   // scaling(1, 1) * x * y
   zisa::array<Scalar, 1> scalings_;
+  zisa::device_type memory_location_ = zisa::device_type::cpu;
 };
 
 #endif // COUPLED_FUNCTION_HPP_
