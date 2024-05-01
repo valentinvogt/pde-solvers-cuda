65b17fda68225551ce57838b3f965532c84bd9ce
Louis Hurschler <louis.hurschler@gmx.ch>
2024-05-01 18:09:48 +0200

    update benchmark script

--- scripts/plot_benchmark.py
+++ scripts/plot_benchmark.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read data from file
-with open('out/res_benchmark_1.txt', 'r') as file:
+with open('out/res_benchmark_5.txt', 'r') as file:
     next(file)  # Skip header
     data = np.loadtxt(file, delimiter=',', skiprows=0)
 
@@ -15,12 +15,14 @@ plt.figure(figsize=(10, 6))
 plt.plot(array_size, time_cpu, marker='o', label='CPU Time')
 plt.plot(array_size, time_cuda, marker='o', label='CUDA Time')
 plt.title('Execution Time Comparison (CPU vs CUDA)')
-plt.xlabel('Array Size')
+plt.xlabel('Number of Iterations')
 plt.ylabel('Time (microseconds)')
-plt.xticks(array_size)
+stepsize = int(len(array_size) / 10.)
+plt.xticks(array_size[::stepsize])
 plt.grid(True)
 plt.legend()
 plt.yscale('log')
+plt.xscale('log')
 
 
-plt.savefig('out/res_benchmark_1.pdf')
+plt.savefig('out/res_benchmark_5.pdf')
