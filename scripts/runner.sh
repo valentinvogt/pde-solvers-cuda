538c06d774b702aeee696eef696c6d93038468ca
Valentin Vogt <vogtva@student.ethz.ch>
2024-11-26 15:37:56 +0100

    adapt runner

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,21 +1,21 @@
-#!/bin/zsh
-#SBATCH --job-name=grow-with-const-ratio
-#SBATCH --output=grow-with-const-ratio-%j.out
-#SBATCH --error=grow-with-const-ratio-%j.err
+#!/bin/bash
+#SBATCH --job-name=vary-dx
+#SBATCH --output=vary-dx-%j.out
+#SBATCH --error=vary-dx-%j.err
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --mem-per-cpu=2048
 #SBATCH --time=01:00:00
 
-# module load stack/2024-06
-# module load gcc/12.2.0
-# module load cmake/3.27.7
-# module load cuda/12.1.1
-# module load hdf5/1.14.3
-# module load openmpi/4.1.6
-# module load netcdf-c/4.9.2
-# module load python/3.11.6
+module load stack/2024-06
+module load gcc/12.2.0
+module load cmake/3.27.7
+module load cuda/12.1.1
+module load hdf5/1.14.3
+module load openmpi/4.1.6
+module load netcdf-c/4.9.2
+module load python/3.11.6
 
 # A: float = 5
 # B: float = 9
@@ -27,7 +27,7 @@
 # Dv: float = 22.0
 # n_snapshots: int = 100
 
-DATAPATH="/cluster/scratch/vogtva/data/vary_dx"
+DATAPATH="/cluster/scratch/vogtva/data/vary-dx"
 
 A=5
 B=9
@@ -35,6 +35,7 @@ Nx=100
 Nt=2_500
 dt=0.0025
 Du=2.0
+Dv=22.0
 n_snapshots=100
 
 for dx in 0.1 0.2 0.3 0.4 0.5 0.75 1.0; do
